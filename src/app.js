cc499e10f4b7b3fe276201798218ce9d35f4d258
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-07-08 11:17:31 +0000

    ultimo corregido

--- src/app.js
+++ src/app.js
@@ -4,37 +4,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  let randomNumber = Math.random() * 10;
-  //write your code here
-  document.querySelector("#The_excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console! " + randomNumber);
-};
+window.onload = function() {
+  function generateExcuse() {
+    var who = ["The dog", "My grandma", "The mailman", "My bird"];
+    var action = ["ate", "peed", "crushed", "broke"];
+    var what = ["my homework", "my phone", "the car"];
+    var when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+    var whoIndex = Math.floor(Math.random() * who.length);
+    var actionIndex = Math.floor(Math.random() * action.length);
+    var whatIndex = Math.floor(Math.random() * what.length);
+    var whenIndex = Math.floor(Math.random() * when.length);
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
+    return (
+      who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+    );
+  }
 
-  return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
-  );
+  function displayExcuse() {
+    var excuse = generateExcuse();
+    console.log(excuse);
+  }
+
+  displayExcuse();
 };
