e110b6134ed22a46acd851c4bbd073c3aec49975
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-07-15 10:59:13 +0000

    final con cambios mejorados

--- src/app.js
+++ src/app.js
@@ -4,43 +4,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateRandomArray(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function generateExcuse() {
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoIndex = generateRandomArray(who);
+  const actionIndex = generateRandomArray(action);
+  const whatIndex = generateRandomArray(what);
+  const whenIndex = generateRandomArray(when);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
+
+function displayExcuse(excuse) {
+  document.getElementById("The_excuse").innerText = excuse;
+  console.log(excuse);
+}
+
+document.getElementById("generate-excuse-btn").addEventListener("click", () => {
+  const excuse = generateExcuse();
+  displayExcuse(excuse);
+});
+
 window.onload = function() {
-  function generateExcuse() {
-    var who = ["The dog", "My grandma", "The mailman", "My bird"];
-    var action = ["ate", "peed", "crushed", "broke"];
-    var what = ["my homework", "my phone", "the car"];
-    var when = [
-      "before the class",
-      "when I was sleeping",
-      "while I was exercising",
-      "during my lunch",
-      "while I was praying"
-    ];
-
-    var whoIndex = Math.floor(Math.random() * who.length);
-    var actionIndex = Math.floor(Math.random() * action.length);
-    var whatIndex = Math.floor(Math.random() * what.length);
-    var whenIndex = Math.floor(Math.random() * when.length);
-
-    return (
-      who[whoIndex] +
-      " " +
-      action[actionIndex] +
-      " " +
-      what[whatIndex] +
-      " " +
-      when[whenIndex]
-    );
-  }
-
-  function displayExcuse() {
-    var excuse = generateExcuse();
-    document.getElementById("The_excuse").innerText = excuse;
-    console.log(excuse);
-  }
-
-  displayExcuse();
-  document
-    .getElementById("generate-excuse-btn")
-    .addEventListener("click", displayExcuse);
+  const excuse = generateExcuse();
+  displayExcuse(excuse);
 };
